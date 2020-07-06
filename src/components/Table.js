2aa01b3ffe6afb055760d945c1d1934d3f2c9037
Marcus <mpt789@gmail.com>
2020-07-06 16:13:04 -0700

    added 75 results instead of 20 in API and  added Table.js

---
+++ src/components/Table.js
@@ -0,0 +1,19 @@
+
+import React from "react";
+
+function Table(props) {
+    return (
+        <tr>
+            <td>
+                <img src={props.photo} alt="employee" />
+            </td>
+            <td>{props.name}</td>
+            <td>{props.email}</td>
+            <td>{props.phone}</td>
+            <td>{props.location}</td>
+            <td>{props.dob}</td>
+        </tr>
+    );
+}
+
+export default Table;
