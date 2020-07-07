6ce9e0459ab7976a68661c59a3cabf134e0262a4
Marcus <mpt789@gmail.com>
2020-07-07 09:40:05 -0700

    Updates

---
+++ precache-manifest.8525e9858d869c3af47c14f162134ebf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6eab0f51d634895d271ecc582720a314",
+    "url": "/react-employee-directory/index.html"
+  },
+  {
+    "revision": "f749ece3a7d33d9450e6",
+    "url": "/react-employee-directory/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "24e7e5a87a5b5801b6c3",
+    "url": "/react-employee-directory/static/js/2.f03ee92c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-employee-directory/static/js/2.f03ee92c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f749ece3a7d33d9450e6",
+    "url": "/react-employee-directory/static/js/main.286f008d.chunk.js"
+  },
+  {
+    "revision": "193cdcbd01f85fb428f8",
+    "url": "/react-employee-directory/static/js/runtime-main.76415422.js"
+  }
+]);
